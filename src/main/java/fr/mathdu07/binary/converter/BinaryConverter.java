b66b7160a6c861690f46c1686706814d2b4d8531
mathdu07 <mathdu07.minecraft@gmail.com>
2014-02-18 20:17:45 +0100

    Make it a maven project

---
+++ src/main/java/fr/mathdu07/binary/converter/BinaryConverter.java
@@ -0,0 +1,17 @@
+package fr.mathdu07.binary.converter;
+
+import java.lang.System;
+
+public class BinaryConverter
+{
+    public static final String VERSION = "0.1-SNAPSHOT";
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Starting BinaryConverter v" + VERSION + " ...");
+    }
+
+}
