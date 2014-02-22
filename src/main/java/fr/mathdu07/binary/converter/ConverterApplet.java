9371d512fb6be53f3240c2dc200e644b4e04b1d3
mathdu07 <mathdu07.minecraft@gmail.com>
2014-02-22 22:39:48 +0100

    Reimplement system's look and feel

--- src/main/java/fr/mathdu07/binary/converter/ConverterApplet.java
+++ src/main/java/fr/mathdu07/binary/converter/ConverterApplet.java
@@ -1,14 +1,16 @@
 package fr.mathdu07.binary.converter;
 
-import java.applet.Applet;
 import java.awt.BorderLayout;
 
+import javax.swing.JApplet;
 import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 import fr.mathdu07.binary.converter.panel.AboutPanel;
 import fr.mathdu07.binary.converter.panel.AsciiPanel;
 
-public class ConverterApplet extends Applet
+public class ConverterApplet extends JApplet
 {
 
     /**
@@ -19,23 +21,42 @@ public class ConverterApplet extends Applet
     private JTabbedPane tabs;
 
     @Override
-    public void destroy()
-    {
-        super.destroy();
-    }
-
-    @Override
     public void init()
     {
         super.init();
+        SwingUtilities.invokeLater(new Runnable() {
+        	public void run()
+        	{
+        		buildApplet();
+        	}
+        });
+    }
+    
+    private void buildApplet()
+    {
         tabs = new JTabbedPane();
         tabs.addTab("ASCII", new AsciiPanel());
         tabs.addTab("À propos", new AboutPanel());
         setLayout(new BorderLayout());
         add(tabs, BorderLayout.CENTER);
+        try
+        {
+        	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        	SwingUtilities.updateComponentTreeUI(this);
+        }
+        catch (Exception e)
+        {
+        	e.printStackTrace();
+        }
     }
 
     @Override
+	public void destroy()
+	{
+	    super.destroy();
+	}
+
+	@Override
     public void start()
     {
         super.start();
